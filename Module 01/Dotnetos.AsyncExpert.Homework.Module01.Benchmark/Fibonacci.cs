41608a3f40dc711436b0d22fe7f6564beed62fe8
Mateusz Redynk <mateusz.redynk@gmail.com>
2020-06-22 11:10:48 +0200

    implementnation of fibonacci algorithms

--- Module 01/Dotnetos.AsyncExpert.Homework.Module01.Benchmark/Fibonacci.cs
+++ Module 01/Dotnetos.AsyncExpert.Homework.Module01.Benchmark/Fibonacci.cs
@@ -3,6 +3,7 @@ using BenchmarkDotNet.Attributes;
 
 namespace Dotnetos.AsyncExpert.Homework.Module01.Benchmark
 {
+	[MemoryDiagnoser]
     [DisassemblyDiagnoser(exportCombinedDisassemblyReport: true)]
     public class FibonacciCalc
     {
@@ -13,6 +14,14 @@ namespace Dotnetos.AsyncExpert.Homework.Module01.Benchmark
         // 4. Open disassembler report and compare machine code
         // 
         // You can use the discussion panel to compare your results with other students
+
+        private Dictionary<ulong, ulong> fibonacciCache;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            fibonacciCache = new Dictionary<ulong, ulong>();
+        }
 
         [Benchmark(Baseline = true)]
         [ArgumentsSource(nameof(Data))]
@@ -26,14 +35,28 @@ namespace Dotnetos.AsyncExpert.Homework.Module01.Benchmark
         [ArgumentsSource(nameof(Data))]
         public ulong RecursiveWithMemoization(ulong n)
         {
-            return 0;
+	        if (fibonacciCache.ContainsKey(n)) return fibonacciCache[n];
+
+	        if (n == 1 || n == 2) return 1;
+
+            fibonacciCache[n] = RecursiveWithMemoization(n - 2) + RecursiveWithMemoization(n - 1);
+	        return fibonacciCache[n];
         }
         
         [Benchmark]
         [ArgumentsSource(nameof(Data))]
         public ulong Iterative(ulong n)
         {
-            return 0;
+	        ulong previous_previous, previous= 0, current = 1;
+	        
+	        for (ulong i = 1; i < n; i++)
+	        {
+		        previous_previous = previous;
+		        previous = current;
+		        current = previous_previous + previous;
+	        }
+
+	        return current;
         }
 
         public IEnumerable<ulong> Data()
